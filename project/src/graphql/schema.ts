5777189b32a6f15b188728d19fb07d4b888796d0
Marcelo Peres <marcelopc04@gmail.com>
2018-08-06 13:12:27 -0300

    graphql

---
+++ project/src/graphql/schema.ts
@@ -0,0 +1,27 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+import { Query } from './query';
+import { Mutation } from './mutation';
+
+
+import { userTypes } from './resources/user/user.schema';
+import { commentTypes } from './resources/comment/comment.schema';
+import { postTypes } from './resources/post/post.schema';
+
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
+    }
+`;
+
+export default makeExecutableSchema({
+    typeDefs:[
+        SchemaDefinition,
+        Query,
+        Mutation,
+        commentTypes,
+        postTypes,
+        userTypes
+    ]
+});
