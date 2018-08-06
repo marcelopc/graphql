59559773eea1d868898497f835f5d9db29faca72
Marcelo Peres <marcelopc04@gmail.com>
2018-08-06 18:02:42 -0300

    resolver post mutation

--- project/src/graphql/resources/post/post.resolvers.ts
+++ project/src/graphql/resources/post/post.resolvers.ts
@@ -2,6 +2,7 @@ import { GraphQLResolveInfo } from "graphql";
 
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { PostInstance } from "../../../models/PostModel";
+import { Transaction } from "../../../../node_modules/@types/sequelize";
 
 export const postResolvers = {
 
@@ -39,6 +40,42 @@ export const postResolvers = {
                         throw new Error(`Post with id ${id} not found!`);
                     return post;
                 });
+        }
+    },
+
+    Mutation: {
+
+        createPost: (parent, {input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post.create(input, {transaction: t});
+            });
+        },
+
+        updatePost:  (parent, {id, input}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if(!post)
+                            throw new Error(`Post with id ${id} not found!`);
+                        return post.update(input, {transaction: t})
+                    });
+            });
+        },
+
+        deletePost:  (parent, {id}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if(!post)
+                            throw new Error(`Post with id ${id} not found!`);
+                        return post.destroy({transaction: t})
+                            .then(post => !!post)
+                    });
+            })
         }
     }
 }
